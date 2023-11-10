1f2981945011551a6555def65f21e1b79dec249b
deepika <deepikatangella1207@gmail.com>
2023-11-10 18:56:57 +0530

    added Mysql.sh       backend.sh

---
+++ Backend.sh
@@ -0,0 +1,22 @@
+dnf module disable nodejs -y
+
+dnf module enable nodejs:18 -y
+
+dnf install nodejs -y
+
+useradd expense
+
+mkdir /app
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+cd /app
+unzip /tmp/backend.zip
+cd /app
+npm install
+
+cp backend.service /etc/systemd/system/backend.service
+
+dnf install mysql -y
+mysql -h <my sql.deepulucky.online> -uroot -pExpenseApp@1 < /app/schema/backend.sql
+systemctl daemon-reload
+systemctl enable backend
+systemctl start backend
